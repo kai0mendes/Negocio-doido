ec95a94b0b2fb412636dff2d8523766b4bec510b
kai0mendes <125167560+kai0mendes@users.noreply.github.com>
2023-03-21 23:42:38 -0300

    Curriculo, contatos e animações

--- static/script.js
+++ static/script.js
@@ -21,4 +21,31 @@ navItem.forEach( item => {
 
 })
 
+/*animando os itens da tela*/
 
+const item = document.querySelectorAll("[data-anime]");
+
+const animeScroll = () => {
+
+   
+
+
+ const windowTop = window.pageYOffset + window.innerHeight * 0.85;
+   
+   
+   
+ item.forEach((element) => {
+      if (windowTop > element.offsetTop ){
+            element.classList.add("animate");
+        }
+        else {
+            element.classList.remove("animate");
+        }
+    });
+};
+
+animeScroll ();
+
+window.addEventListener("scroll", ()=>{
+    animeScroll();
+})
