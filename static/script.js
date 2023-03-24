9f3403b2aff8611f1014f9f8e77e8814f5cd1ca3
kai0mendes <125167560+kai0mendes@users.noreply.github.com>
2023-03-24 17:22:32 -0300

    footer and config deploy

--- static/script.js
+++ static/script.js
@@ -49,3 +49,18 @@ animeScroll ();
 window.addEventListener("scroll", ()=>{
     animeScroll();
 })
+
+/*botão enviar*/
+
+
+const btnEnviar = document.querySelector('#btn-enviar')
+const btnEnviarLoader = document.querySelector('#btn-enviar-loader')
+
+btnEnviar.addEventListener("click", ()=>{
+    btnEnviarLoader.style.display = "block";
+    btnEnviar.style.display = "none"
+})
+
+setTimeout(() => {
+    document.querySelector('#alerta').style.display = 'none';
+}, 5000);
