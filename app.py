96fc9fb652a984974d72110881af0746d8e5c2f1
kai0mendes <125167560+kai0mendes@users.noreply.github.com>
2023-03-25 15:20:09 -0300

    ddd

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ mail_settings = {
     "MAIL_USE_TLS": False,
     "MAIL_USE_SSL": True,
     "MAIL_USERNAME": os.getenv("EMAIL"),
-    "MAIL_PASSWORD": os.getenv("SENHA"),
+    "MAIL_PASSWORD": os.getenv("SENHA")
 }
 
 app.config.update(mail_settings)
